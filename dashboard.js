ebb7850f3bb164a3a6efbb9605c0140681fa112a
giacomo.manzo <giacomo.manzo99@gmail.com>
2021-08-03 17:17:05 +0200

    a
    Signed-off-by: giacomo.manzo <giacomo.manzo99@gmail.com>

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,5 @@
-let date = new Date()
-document.querySelector('.date').textContent = `${date.toDateString()} ${date.getHours()}:${date.getMinutes()}`
-
+for (let i = 0; i < 2; i++) {
+  let clone = document.querySelector(".cards").cloneNode(true);
+  clone.setAttribute("id", "newId");
+  document.querySelector(".slider").appendChild(clone);
+}
