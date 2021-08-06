d534b85801893fd6f1e621a94eb0c68a2ebe618b
giacomo.manzo <giacomo.manzo99@gmail.com>
2021-08-06 11:34:26 +0200

    final version

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,47 @@
-for (let i = 0; i < 2; i++) {
-  let clone = document.querySelector(".cards").cloneNode(true);
-  clone.setAttribute("id", "newId");
-  document.querySelector(".slider").appendChild(clone);
-}
+const cardsList = document.querySelector(".swiper-wrapper");
+
+const loadCards = () => {
+  try {
+    fetch("http://localhost:3001/initiatives")
+      .then((res) => res.json())
+      .then((res) => displayProducts(res))
+      .then((res) => {
+        const swiper = new Swiper(".mySwiper", {
+          slidesPerView: 2.5,
+          spaceBetween: 20,
+          pagination: {
+            el: ".slider-pagination",
+            clickable: true,
+          },
+        });
+      });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const displayProducts = (items) => {
+  const htmlString = items
+    .map((item) => {
+      return `
+          <div class="swiper-slide blur" role="group">
+        <div class="mini-card">
+          <p class="duration">12.36 min</p>
+          <img src="images/elements-icon-podcast.svg" class="podcast" />
+        </div>
+        <div class="cards-sub-container">
+          <p class="txt-cards1">${item.date}</p>
+          <p class="txt-cards2">${item.title}</p>
+          <p class="txt-cards1">
+            ${item.description}
+          </p>
+        </div>
+      </div>    
+      `;
+    })
+    .join("");
+  cardsList.innerHTML = htmlString;
+  return true;
+};
+
+loadCards();
